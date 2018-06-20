8c3081ef72dd715147ad1cca7c03ad5d00123b6f
abhay rajpal <arajpal@bestpeers.com>
2018-06-20 14:46:43 +0530

    completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # include DeviseTokenAuth::Concerns::SetUserByToken
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   skip_before_action :verify_authenticity_token
   # protect_from_forgery with: :null_session
 end
